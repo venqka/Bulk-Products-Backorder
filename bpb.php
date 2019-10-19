c63115743f9365279ebc0ae2f7435f2a52b69d4f
venelinb <venelinborisov5@gmail.com>
2019-10-19 19:18:35 +0300

    Create bulk products backorder settings page

--- bpb.php
+++ bpb.php
@@ -23,3 +23,20 @@ function bpb_enqueue_scripts() {
   wp_localize_script( 'bpb', 'bpb_ajax', $ajax_args );
 }
 add_action( 'admin_enqueue_scripts', 'bpb_enqueue_scripts', 30 );
+
+function bpb_create_menu() {
+
+  add_menu_page( 'Bulk Products Backorder', 'Bulk Products Backorder', 'administrator', 'bulk-products-backorder', 'bulk_products_backorder' , 'dashicons-admin-tools' );
+}
+add_action( 'admin_menu', 'bpb_create_menu' );
+
+
+function bulk_products_backorder() {
+?>
+<div class="wrap">
+  <h1>Bulk Products Backorder</h1>
+  <p>This plugin sets backorder to all products with one click.</p>
+  <button class="button button-primary" id="bpb-trigger">Set backorder to all products</button>
+</div>
+<div class="response"></div>  
+<?php }
